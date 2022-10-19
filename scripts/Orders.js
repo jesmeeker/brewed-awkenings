7c4a3cf619b2467316792ec5f1417f35f1b0c9e6
Jessica Meeker <jesmeeker@gmail.com>
2022-10-19 11:57:07 -0500

    debugged

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -23,7 +23,7 @@ const findProduct = (order, allProducts) => {
 const findEmployee = (order, allEmployees) => {
     let orderEmployee = null
 
-    for (const employee in allEmployees) {
+    for (const employee of allEmployees) {
         if (employee.id === order.employeeId) {
             orderEmployee = employee
         }
@@ -38,9 +38,9 @@ export const Orders = () => {
 
     for (const order of orders) {
         const employee = findEmployee(order, employees)
-        const product = findProduct(order)
+        const product = findProduct(order, products)
 
-        html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
+        html += `<li>${product.name} was sold by ${employee.name}on ${new Date(order.timestamp).toLocaleDateString()}</li>`
     }
 
     html += "</ul>"
