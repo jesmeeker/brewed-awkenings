263703cdbf37a87a5126007b65300ae72abb4d0f
Jessica Meeker <jesmeeker@gmail.com>
2022-10-19 14:26:39 -0500

    eventlistener with number of orders sold

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,7 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
@@ -14,3 +15,52 @@ export const Employees = () => {
     return html
 }
 
+// document.addEventListener(
+//     "click",  // This is the type of event
+//     (clickEvent) => {
+       
+//         const itemClicked = clickEvent.target
+
+//         if (itemClicked.id.startsWith("employee")) {
+
+//             const [,employeeId] = itemClicked.id.split("--")
+
+//             for (const employee of employees) {
+//                 if (employee.id === parseInt(employeeId)) {
+//                     //window alert needs to say ${employee.name} has had {{blank number}} of sales
+//                     //will need a way to find total number of sales for each empl
+//                     window.alert(`${employee.name} has sold $${employee.email}.`)
+//                 }
+//             }
+//          }
+//     }
+// )
+
+document.addEventListener(
+    "click",  
+    (clickEvent) => {
+        
+        const itemClicked = clickEvent.target
+        
+        if (itemClicked.id.startsWith("employee")) {
+            
+            let employeeOrders = []
+            const [,employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                let orderCount = "0"
+                for (const order of orders) {
+                    if (employee.id === order.employeeId) {
+                        employeeOrders.push(employee)
+                        orderCount = employeeOrders.length
+                    }
+                        //window alert needs to say ${employee.name} has had {{blank number}} of sales
+                        //will need a way to find total number of sales for each employee
+                    }
+                    window.alert(`${employee.name} has sold ${orderCount} orders.`)
+                }
+            }
+         }
+    }
+)
--- scripts/Products.js
+++ scripts/Products.js
@@ -14,3 +14,23 @@ export const Products = () => {
     return html
 }
 
+document.addEventListener(
+    "click",  // This is the type of event
+    (clickEvent) => {
+       
+        const itemClicked = clickEvent.target
+
+        if (itemClicked.id.startsWith("product")) {
+
+            const [,productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+           
+                    window.alert(`${product.name} costs $${product.price}.`)
+                }
+            }
+         }
+    }
+)
+
